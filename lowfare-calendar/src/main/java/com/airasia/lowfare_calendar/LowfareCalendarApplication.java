9b6a7b699604cb95c6d199cdfcee5e3510c69b5d
divij nath <divij.nath6011gmail.com@Macbook-air-m2.local>
2026-05-22 21:36:54 +0530

    create project structure

---
+++ lowfare-calendar/src/main/java/com/airasia/lowfare_calendar/LowfareCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.airasia.lowfare_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowfareCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowfareCalendarApplication.class, args);
+	}
+
+}
